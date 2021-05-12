eaf3250e7b0b927fc7d3f13a610efd69f0ffb326
yurii <selebinkayuriy@gmail.com>
2021-05-12 18:59:48 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+/**
+ * @author Yurii Selebynka
+ * @version alpha-0.0.1
+ * @project TCMessenger
+ * @class com.company
+ * @since 12-May-21 - 18.58
+ *
+ * Copyright (c) 1996-present Yurii Selebynka. All Rights Reserved.
+ * UR1DAO
+ *
+ **/
+
+public class Main {
+
+    public static void main(String[] args) {
+	// this is only the beginning)
+    }
+}
